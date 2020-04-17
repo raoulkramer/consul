48d747563cb375a4ce68d7623a57998149713906
abihas <abihas@server6.colo.transip.net>
2020-04-17 23:21:47 +0200

    Change to production env

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,8 +10,8 @@ usermod -u "$USER_UID" consul 2> /dev/null
 groupmod -g "$USER_GID" consul 2> /dev/null
 usermod -g "$USER_GID" consul 2> /dev/null
 
-chown -R -h "consul" "/usr/local/bundle"
-chgrp -R -h "consul" "/usr/local/bundle"
+chown -R -h "$USER_UID" "$BUNDLE_PATH"
+chgrp -R -h "$USER_GID" "$BUNDLE_PATH"
 
 /usr/bin/sudo -EH -u consul "$@"
 
