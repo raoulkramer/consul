5ef99e3401abb24f68b1e40ce364194333949ec0
abihas <info@michaelnieuwenweg.nl>
2020-05-05 21:56:24 +0200

    limit nr of languages

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,10 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :ka
+    available_locales = ["ka","en"]
+    config.i18n.available_locales = available_locales
+    config.i18n.fallbacks = {
+      "ka"    => "en"
+    }
   end
 end
