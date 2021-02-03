75e13dd467745ff551f9511ab36b5daf02c4e9ba
Javi Martín <javim@elretirao.net>
2021-02-03 18:10:42 +0100

    Make box-shadow in cards permanent
    We were using it on hover because the cards were links. However,
    displaying it all the time meant cards were overlapping.
    
    So we have to add a gap between cards, but then we also have to
    recalculate the width so the gap is taken into account. Since the gap
    depends on the `card.columns` attribute, we're including it in a HTML
    data attribute.

--- spec/system/admin/widgets/cards_spec.rb
+++ spec/system/admin/widgets/cards_spec.rb
@@ -56,9 +56,9 @@ describe "Cards" do
 
     visit root_path
 
-    expect(page).to have_css("#widget_card_#{card_1.id}.medium-8")
-    expect(page).to have_css("#widget_card_#{card_2.id}.medium-4")
-    expect(page).to have_css("#widget_card_#{card_3.id}.medium-2")
+    expect(page).to have_css("#widget_card_#{card_1.id}[data-columns='8']")
+    expect(page).to have_css("#widget_card_#{card_2.id}[data-columns='4']")
+    expect(page).to have_css("#widget_card_#{card_3.id}[data-columns='2']")
   end
 
   scenario "Edit" do
@@ -163,9 +163,9 @@ describe "Cards" do
 
         expect(page).to have_css(".card", count: 3)
 
-        expect(page).to have_css("#widget_card_#{card_1.id}.medium-8")
-        expect(page).to have_css("#widget_card_#{card_2.id}.medium-4")
-        expect(page).to have_css("#widget_card_#{card_3.id}.medium-2")
+        expect(page).to have_css("#widget_card_#{card_1.id}[data-columns='8']")
+        expect(page).to have_css("#widget_card_#{card_2.id}[data-columns='4']")
+        expect(page).to have_css("#widget_card_#{card_3.id}[data-columns='2']")
       end
 
       scenario "Show label only if it is present" do
